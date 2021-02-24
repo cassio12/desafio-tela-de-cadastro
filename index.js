825208c3a536774455ea102c0e6a7d21ad35824f
user <cassio12>
2021-02-24 18:31:08 -0300

    transição entre as telas de login e cadastro finalisadas

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let keyLogin = document.getElementById('login')
 let keyRegister = document.getElementById('register')
 
+let loginTransform = document.getElementById('loginTransform')
+let lineAnimetion = document.getElementById('line_animetion')
+
 let screenregister = document.getElementById('registerSection')
 let screenLogin = document.getElementById('loginSection')
 
@@ -11,6 +14,9 @@ let main = () => {
         if (screenregister.style.display === ''){
             screenregister.style.display = 'none'
             screenLogin.style.display = 'block'
+            loginTransform.setAttribute('class', 'transicao1')
+            keyRegister.setAttribute('class', 'transicao2')
+            lineAnimetion.setAttribute('class', 'direction_page')
         } else {
             screenregister.style.display = ''
             screenLogin.style.display = 'none'
@@ -22,6 +28,8 @@ let main = () => {
         if (screenregister.style.display === 'none'){
             screenregister.style.display = 'block'
             screenLogin.style.display = 'none'
+            loginTransform.setAttribute('class', 'box_subtitleActionLogin--txt_login')
+            keyRegister.setAttribute('class', 'box_subtitleActionLogin--txt_itenRegister')
         } else {
             screenregister.style.display = 'none'
             screenLogin.style.display = 'block'
